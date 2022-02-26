c8541f7fd9e9e61dfed3a75075ff8cbed9d785e1
AnsariQamar <ssky5021@gmail.com>
2022-02-26 12:05:40 +0530

    readme file

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -64,7 +64,7 @@ export default function Signup() {
     <>
       {loading ? (
         <div className={styles.load}>
-          <TailSpin color="#00BFFF" height={200} width={80} />
+          <TailSpin color="#00BFFF" height={200} width={200} />
           <p>Loading.....</p>
         </div>
       ) : (
